49005885c745b49f06d2788770b618997a109265
caiovecchiati <caioriq@hotmail.com>
2023-01-10 22:12:21 -0300

    Adicionado repositório para os Pedidos e adicionado função de busca de lanches.

---
+++ LanchesMac/Repositories/PedidoRepository.cs
@@ -0,0 +1,40 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+
+namespace LanchesMac.Repositories
+{
+    public class PedidoRepository : IPedidoRepository
+    {
+        private readonly AppDbContext _appDbcontext;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoRepository(AppDbContext appDbcontext, CarrinhoCompra carrinhoCompra)
+        {
+            _appDbcontext = appDbcontext;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public void CriarPedido(Pedido pedido)
+        {
+            pedido.PedidoEnviado = DateTime.Now;
+            _appDbcontext.Pedidos.Add(pedido);
+            _appDbcontext.SaveChanges();
+
+            var carrinhoCompraItens = _carrinhoCompra.CarrinhoCompraItems;
+
+            foreach (var carrinhoItem in carrinhoCompraItens)
+            {
+                var pedidoDetail = new PedidoDetalhe()
+                {
+                    Quantidade = carrinhoItem.Quantidade,
+                    LancheId = carrinhoItem.Lanche.LancheId,
+                    PedidoId = pedido.PedidoId,
+                    Preco = carrinhoItem.Lanche.Preco
+                };
+                _appDbcontext.PedidoDetalhes.Add(pedidoDetail);
+            }
+            _appDbcontext.SaveChanges();
+        }
+    }
+}
